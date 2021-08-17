991d41f0cc1646d711860a632aa5b19d0e78cfa4
venkatab144 <37525611+venkatab144@users.noreply.github.com>
2021-08-17 13:23:09 +1000

    moved header files fixed header text moving on scroll

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <h1>Test</h1>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -68,7 +68,7 @@ const Header = () => {
       <div>
           <motion.svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 100 100"
+              viewBox="0 0 100 20"
               className="item"
             >
               <motion.path
