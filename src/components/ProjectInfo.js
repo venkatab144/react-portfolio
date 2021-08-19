15c11be7ff0c0e0fdc65519ac6f2145a701e69ee
venkatab144 <37525611+venkatab144@users.noreply.github.com>
2021-08-20 00:19:21 +1000

    added academic projects

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -5,37 +5,37 @@ export const AcademicProjects = {
             {
                 id: 1,
                 title: "Engineering Capstone: Coral Reef Detection and Classification (ongoing)",
-                body: "Maecenas semper nisi sed lobortis condimentum. Vestibulum imperdiet dui non elementum semper. Nam ornare ac augue sit amet ornare. Donec luctus consectetur consectetur. Vestibulum ante quam, dignissim sed sem sed, eleifend scelerisque erat. Sed et dignissim urna. Donec quis diam tortor. Suspendisse potenti. Ut viverra, diam ac ultricies blandit, ante nunc congue orci, accumsan laoreet felis odio et mi. In hac habitasse platea dictumst. Cras lacinia fringilla viverra. Quisque nulla neque, hendrerit quis ipsum ut, faucibus bibendum odio. Aenean faucibus rhoncus lacus, vitae volutpat tellus vulputate facilisis. Etiam sed risus dui. Nunc tincidunt vulputate efficitur."
+                body: "Currently, no solution exists that efficiently and automatically analyzes imagery of coral and identifies the species as well as identify their level of health; healthy, unhealthy (diseased), damaged (physically), bleached and dead. Coral imagery data is collected from various different sources, labelled and compiled – pre-labelled datasets can also be used. This data will then be used to train a Convolutional Neural Network (CNN) to automatically classify the species of coral. Coral health imagery data will also be collected. This health data will be used to train the CNN to automatically classify coral health. This project will be built using python, tensorflow and keras."
             }, 
             {
                 id: 3,
                 title: "EPR2 - E-Portfolio (ongoing)",
-                body: "Maecenas semper nisi sed lobortis condimentum. Vestibulum imperdiet dui non elementum semper. Nam ornare ac augue sit amet ornare. Donec luctus consectetur consectetur. Vestibulum ante quam, dignissim sed sem sed, eleifend scelerisque erat. Sed et dignissim urna. Donec quis diam tortor. Suspendisse potenti. Ut viverra, diam ac ultricies blandit, ante nunc congue orci, accumsan laoreet felis odio et mi. In hac habitasse platea dictumst. Cras lacinia fringilla viverra. Quisque nulla neque, hendrerit quis ipsum ut, faucibus bibendum odio. Aenean faucibus rhoncus lacus, vitae volutpat tellus vulputate facilisis. Etiam sed risus dui. Nunc tincidunt vulputate efficitur."
+                body: "For my EPR2 assignment, I was tasked with creating an E-Portfolio and I had the option to make it myself or use tools such as Wix and Wordpress. I opted to create the site myself and developed the entire site in React and so I made this website."
             },
             {
                 id: 5,
                 title: "Software Studio 3B - UTS Event Organiser App",
-                body: "Maecenas semper nisi sed lobortis condimentum. Vestibulum imperdiet dui non elementum semper. Nam ornare ac augue sit amet ornare. Donec luctus consectetur consectetur. Vestibulum ante quam, dignissim sed sem sed, eleifend scelerisque erat. Sed et dignissim urna. Donec quis diam tortor. Suspendisse potenti. Ut viverra, diam ac ultricies blandit, ante nunc congue orci, accumsan laoreet felis odio et mi. In hac habitasse platea dictumst. Cras lacinia fringilla viverra. Quisque nulla neque, hendrerit quis ipsum ut, faucibus bibendum odio. Aenean faucibus rhoncus lacus, vitae volutpat tellus vulputate facilisis. Etiam sed risus dui. Nunc tincidunt vulputate efficitur."
+                body: "In Software Studio 3B, our team created a UTS Event Organiser App using React, C# and MongoDB. The website had basic functionality that allowed for the addition of various types of events, authentication for different users and a messaging feature as well."
             },
             {
                 id: 7,
                 title: "Software Studio 3A - AR Directions App",
-                body: "Maecenas semper nisi sed lobortis condimentum. Vestibulum imperdiet dui non elementum semper. Nam ornare ac augue sit amet ornare. Donec luctus consectetur consectetur. Vestibulum ante quam, dignissim sed sem sed, eleifend scelerisque erat. Sed et dignissim urna. Donec quis diam tortor. Suspendisse potenti. Ut viverra, diam ac ultricies blandit, ante nunc congue orci, accumsan laoreet felis odio et mi. In hac habitasse platea dictumst. Cras lacinia fringilla viverra. Quisque nulla neque, hendrerit quis ipsum ut, faucibus bibendum odio. Aenean faucibus rhoncus lacus, vitae volutpat tellus vulputate facilisis. Etiam sed risus dui. Nunc tincidunt vulputate efficitur."
+                body: "In Software Studio 3A, our team developed an AR Directions App. I used Unity, Mapbox and World-scale AR to develop the app. It used the GPS and identified features such as roads and buildings to locate the users position and heading. It then overlayed the directions on the camera screen."
             },
             {
                 id: 9,
                 title: "Software Studio 2A & 2B - UTS HELPS Concept Website",
-                body: "Maecenas semper nisi sed lobortis condimentum. Vestibulum imperdiet dui non elementum semper. Nam ornare ac augue sit amet ornare. Donec luctus consectetur consectetur. Vestibulum ante quam, dignissim sed sem sed, eleifend scelerisque erat. Sed et dignissim urna. Donec quis diam tortor. Suspendisse potenti. Ut viverra, diam ac ultricies blandit, ante nunc congue orci, accumsan laoreet felis odio et mi. In hac habitasse platea dictumst. Cras lacinia fringilla viverra. Quisque nulla neque, hendrerit quis ipsum ut, faucibus bibendum odio. Aenean faucibus rhoncus lacus, vitae volutpat tellus vulputate facilisis. Etiam sed risus dui. Nunc tincidunt vulputate efficitur."
+                body: "In Software Studio 2A & 2B, our team developed a concept website for UTS HELPS that we presented to a representative from UTS HELPS. The website featured a scheduling system with event organisation as well as user authentication and the ability for custom admin control."
             },
             {
                 id: 11,
                 title: "Software Studio 1B - Health Tracker App",
-                body: "Maecenas semper nisi sed lobortis condimentum. Vestibulum imperdiet dui non elementum semper. Nam ornare ac augue sit amet ornare. Donec luctus consectetur consectetur. Vestibulum ante quam, dignissim sed sem sed, eleifend scelerisque erat. Sed et dignissim urna. Donec quis diam tortor. Suspendisse potenti. Ut viverra, diam ac ultricies blandit, ante nunc congue orci, accumsan laoreet felis odio et mi. In hac habitasse platea dictumst. Cras lacinia fringilla viverra. Quisque nulla neque, hendrerit quis ipsum ut, faucibus bibendum odio. Aenean faucibus rhoncus lacus, vitae volutpat tellus vulputate facilisis. Etiam sed risus dui. Nunc tincidunt vulputate efficitur."
+                body: "In Software Studio 1B, our team developed an Android Health Tracker app that tracked various statistics such as Heart Rate, Sleep, etc... I worked on the Heart Rate tracker which used the camera and flash light to detect the heart rate of the user. I also worked on the step tracker which allowed the user to track their steps."
             },
             {
                 id: 13,
                 title: "Software Studio 1A - Fitness App",
-                body: "Maecenas semper nisi sed lobortis condimentum. Vestibulum imperdiet dui non elementum semper. Nam ornare ac augue sit amet ornare. Donec luctus consectetur consectetur. Vestibulum ante quam, dignissim sed sem sed, eleifend scelerisque erat. Sed et dignissim urna. Donec quis diam tortor. Suspendisse potenti. Ut viverra, diam ac ultricies blandit, ante nunc congue orci, accumsan laoreet felis odio et mi. In hac habitasse platea dictumst. Cras lacinia fringilla viverra. Quisque nulla neque, hendrerit quis ipsum ut, faucibus bibendum odio. Aenean faucibus rhoncus lacus, vitae volutpat tellus vulputate facilisis. Etiam sed risus dui. Nunc tincidunt vulputate efficitur."
+                body: "In Software Studio 1A, our team developed a Fitness Android app. The app had various features such as the run tracker which tracked the user running path as well as time and other details. In addition to this I developed the save feature which saved all the data as a packet to a Google Drive and also displayed it."
             },
         ]
 }
